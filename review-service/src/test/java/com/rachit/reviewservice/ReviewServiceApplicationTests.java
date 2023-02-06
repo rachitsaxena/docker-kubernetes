75e7a3209b1557b27a3d52d8a0738d903bc59cf4
Rachit <rachit.id@gmail.com>
2023-02-06 13:17:30 +0530

    Initial draft

---
+++ review-service/src/test/java/com/rachit/reviewservice/ReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rachit.reviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
